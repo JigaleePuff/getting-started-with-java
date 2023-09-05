b4bb318581eb41dca32fbfe61bbee312435d764a
Jigalee Puff <marsayah@gmail.com>
2023-09-05 23:21:12 +0100

    redoing getting started with java exercise have not managed to complile Helloworld.java/Helloworld.class

---
+++ com/cbfacademy/Helloworld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+/*ive gotten to line 42 in the readme for "getting started with java" ive not figured out how to complie, seems as though it has created a seperate file instead of being under SRC. */
